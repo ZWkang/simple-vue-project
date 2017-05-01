f73a0f1415fdc23d0269e76bd9eacf7aaf2d26cc
ZWkang <907747874@qq.com>
2017-05-01 20:23:04 +0800

    edit router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,48 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-
+// import Hello from '@/components/Hello'
+// import topic from '@/components/topic'
+import login from '@/components/Login'
+import self from '@/components/Self'
+import about from '@/components/About'
+import detail from '@/components/Detail'
+import list from '@/components/List'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'home',
+      component: list
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: about
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/list/:artcle',
+      name: 'list',
+      component: list
+    },
+    {
+      path: '/self/:loginname',
+      name: 'self',
+      component: self
+    },
+    {
+      path: '/detail/:id',
+      name: 'detail',
+      component: detail
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
