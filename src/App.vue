6fd5729c237556078115021730abd4dc4ae91a6b
ZWkang <907747874@qq.com>
2017-05-02 09:54:16 +0800

    delete #app color

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,5 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
 }
 </style>
