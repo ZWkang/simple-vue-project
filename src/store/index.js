cbcb56cc02df0b27768f6ebdb801c6d2eeaec3a4
ZWkang <907747874@qq.com>
2017-05-05 21:18:00 +0800

    修改了之前重名问题

--- src/store/index.js
+++ src/store/index.js
@@ -5,17 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    loginstate: false,
+    islogin: localStorage.getItem('islogin') || false,
     user: {
-      'loginname': '',
-      'avatar_url': '',
-      'id': '',
-      'accesstoken': ''
+      'loginname': localStorage.getItem('loginname') || '',
+      'avatar_url': localStorage.getItem('avatar_url') || '',
+      'id': localStorage.getItem('id') || '',
+      'accesstoken': localStorage.getItem('accesstoken') || 'e2d5e960-fcb8-4302-9a2a-9aa788cdb19d'
     }
   },
   getters: {
     getLoginState: state => {
-      return state.loginstate
+      return state.islogin
     },
     loginstate: state => {
       return state.user
@@ -24,15 +24,35 @@ export default new Vuex.Store({
   actions: {
     LOGIN: function ({ commit }, userinfoobj) {
       commit('SET_USER_INFO', {userinfoobj})
+    },
+    LOGOUT: function ({ commit }) {
+      commit('LOGOUT_CLEAR')
     }
   },
   mutations: {
     SET_USER_INFO: (state, {userinfoobj}) => {
-      state.loginstate = true
+      state.islogin = true
       state.user.loginname = userinfoobj.loginname
       state.user.avatar_url = userinfoobj.avatar_url
       state.user.id = userinfoobj.id
       state.user.accesstoken = userinfoobj.accesstoken
+      localStorage.setItem('islogin', true)
+      localStorage.setItem('loginname', userinfoobj.loginname)
+      localStorage.setItem('avatar_url', userinfoobj.avatar_url)
+      localStorage.setItem('id', userinfoobj.id)
+      localStorage.setItem('accesstoken', userinfoobj.accesstoken)
+    },
+    LOGOUT_CLEAR: (state) => {
+      state.islogin = false
+      state.user.loginname = ''
+      state.user.avatar_url = ''
+      state.user.id = ''
+      state.user.accesstoken = ''
+      localStorage.setItem('islogin', false)
+      localStorage.setItem('loginname', '')
+      localStorage.setItem('avatar_url', '')
+      localStorage.setItem('id', '')
+      localStorage.setItem('accesstoken', '')
     }
   }
 })
